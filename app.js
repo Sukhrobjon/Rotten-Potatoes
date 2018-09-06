7b8c17ed9be31039e0d489d252bbfaeb340d7aaf
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-09-06 12:21:42 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`); // redirect to reveiws/:id
   }).catch((err) => {
     console.log(err.message);
   })
@@ -51,7 +51,14 @@ app.post('/reviews', (req, res) => {
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
 })
-
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
