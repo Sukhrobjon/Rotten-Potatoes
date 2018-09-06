cb171c74d72211ca8a33fc449e86f0a0c7ab74fa
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-09-06 10:40:23 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//MIDDLEWARE
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 
+//MODEL
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String,
+  rating: Number
+});
 
-// INDEX
+// Index
 app.get('/', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -17,27 +37,22 @@ app.get('/', (req, res) => {
     })
 })
 
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
-const Review = mongoose.model('Review', {
-  title: String
-});
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
-
-
-
-
-// OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//   { title: "Great Review" },
-//   { title: "Next Review" }
-// ]
-
-// INDEX
-app.get('/reviews', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
-})
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
