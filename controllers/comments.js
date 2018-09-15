664786bf1a1813139a6537a91a778dda47cb29da
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-09-15 16:11:55 -0700

    all CRUD working perfectly fine for both comments and reviews

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,26 +1,27 @@
 const Comment = require('../models/comment.js');
-
+const Review = require('../models/review.js')
 module.exports = function (app) {
 
-
 // CREATE Comment
-app.post('/reviews/comments', (req, res) => {
+app.post('/movies/:movieId/reviews/:id/comments', (req, res) => {
     Comment.create(req.body).then(comment => {
-        res.redirect(`/reviews/${comment.reviewId}`)
+        res.redirect(`/movies/:movieId/reviews/${comment.reviewId}`)
     }).catch((err) => {
         console.log(err.message)
     })
 });
 
-// DELETE
-app.delete('/reviews/comments/:id', function (req, res) {
-    console.log("DELETE comment")
-        Comment.findByIdAndRemove(req.params.id).then((comment) => {
-            res.redirect(`/reviews/${comment.reviewId}`);
-        }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
+    // DELETE
+    app.delete('/movies/:moviesId/reviews/:reviewsId/comments/:id', function (req, res) {
+        console.log("DELETE comment")
+            Comment.findByIdAndRemove(req.params.id).then((comment) => {
+                res.redirect(`/movies/:moviesId/reviews/${comment.reviewId}`);
+            }).catch((err) => {
+        console.log(err.message);
+        })
+    });
 
 };
+
+
+
